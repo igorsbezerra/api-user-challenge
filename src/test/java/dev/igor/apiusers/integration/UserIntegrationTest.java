9d19b7b9ca2c9c3c54c3d07f5a349830378b170d
Igor de Souza Bezerra <igorbezerra01@outlook.com>
2024-01-15 15:32:14 -0300

    refactor test and add new tests

--- src/test/java/dev/igor/apiusers/integration/UserIntegrationTest.java
+++ src/test/java/dev/igor/apiusers/integration/UserIntegrationTest.java
@@ -1,5 +1,8 @@
 package dev.igor.apiusers.integration;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -12,8 +15,12 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import dev.igor.apiusers.model.User;
+import dev.igor.apiusers.repository.UserRepository;
 
 @SpringBootTest
 @ActiveProfiles(profiles = "test")
@@ -22,15 +29,11 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 public class UserIntegrationTest {
     @Autowired MockMvc mockMvc;
     @Autowired ObjectMapper mapper;
+    @Autowired UserRepository userRepository;
 
     @Test
     void should_be_possible_to_run_an_integrated_test_to_create_a_user() throws Exception {
-        ObjectNode objectNode = mapper.createObjectNode();
-        objectNode.put("name", "Igor");
-        objectNode.put("document", "34686598650");
-        objectNode.put("address", "Sao Paulo");
-        objectNode.put("password", "123456");
-        String json = mapper.writeValueAsString(objectNode);
+        String json = createUserJson();
 
         mockMvc.perform(
             MockMvcRequestBuilders.post("/users")
@@ -40,18 +43,59 @@ public class UserIntegrationTest {
     }
 
     @Test
-    void should_not_be_possible_to_run_an_integrated_test_to_create_a_user() throws Exception {
-        ObjectNode objectNode = mapper.createObjectNode();
-        objectNode.put("name", "");
-        objectNode.put("document", "");
-        objectNode.put("address", "");
-        objectNode.put("password", "");
-        String json = mapper.writeValueAsString(objectNode);
+    void should_not_be_possible_to_run_an_integrated_test_to_create_a_user_when_to_send_empty_value_properties() throws Exception {
+        String json = createEmptyUserJson();
 
         mockMvc.perform(
             MockMvcRequestBuilders.post("/users")
             .contentType(MediaType.APPLICATION_JSON_VALUE)
             .content(json)
         ).andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    void should_not_be_possible_to_run_an_integrated_test_to_create_a_user_when_not_to_send_body() throws Exception {
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/users")
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+        ).andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    @Test
+    void should_not_be_possible_to_run_an_integrated_test_to_create_a_user_when_user_exists() throws Exception {
+        String json = createUserJson();
+        User user = new User(
+            UUID.randomUUID().toString(),
+            "Igor",
+            "34686598650",
+            "Sao Paulo",
+            "123456",
+            LocalDateTime.now()
+        );
+        userRepository.save(user);
+
+        mockMvc.perform(
+            MockMvcRequestBuilders.post("/users")
+            .contentType(MediaType.APPLICATION_JSON_VALUE)
+            .content(json)
+        ).andExpect(MockMvcResultMatchers.status().isBadRequest());
+    }
+
+    private String createUserJson() throws JsonProcessingException {
+        ObjectNode objectNode = mapper.createObjectNode();
+        objectNode.put("name", "Igor");
+        objectNode.put("document", "34686598650");
+        objectNode.put("address", "Sao Paulo");
+        objectNode.put("password", "123456");
+        return mapper.writeValueAsString(objectNode);
+    }
+
+    private String createEmptyUserJson() throws JsonProcessingException {
+        ObjectNode objectNode = mapper.createObjectNode();
+        objectNode.put("name", "");
+        objectNode.put("document", "");
+        objectNode.put("address", "");
+        objectNode.put("password", "");
+        return mapper.writeValueAsString(objectNode);
     }
 }
